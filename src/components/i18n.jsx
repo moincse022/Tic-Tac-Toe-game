6c2bec9b67d6832288be267647f84bd00128a014
Moinuddin-hub <moincse022@gmail.com>
2024-10-02 22:51:24 +0600

    language add done

---
+++ src/components/i18n.jsx
@@ -0,0 +1,40 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+// Translation resources
+const resources = {
+    en: {
+      translation: {
+        title: "Tic Tac Toe Game",
+        welcome: "Welcome to the Game!",
+        nextPlayer: "Next Player: {{player}}",
+        reset: "Reset Game",
+        congratulations: "Congratulations!",
+        winner: "Winner",
+        OK: "OK",
+      },
+    },
+    bn: {
+      translation: {
+        title: "টিক ট্যাক টো গেম",
+        welcome: "গেমে স্বাগতম!",
+        nextPlayer: "পরবর্তী খেলোয়াড়: {{player}}",
+        reset: "গেম রিসেট",
+        congratulations: "অভিনন্দন!",
+        winner: "বিজয়ী",
+        OK: "ঠিক আছে",
+      },
+    },
+  };
+  
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'en', // default language
+  fallbackLng: 'en', // fallback language
+  interpolation: {
+    escapeValue: false, // React already does escaping
+  },
+});
+
+export default i18n;
